6e1497623bd508fcdc1f7a9cc7909445fc745ebe
sandeep bhaskar <sandeep.bhaskar@us.gt.com>
2023-02-16 14:31:44 +0530

    added apis for match service.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-import * as dotenv from 'dotenv';
-import { getData } from './services/MatchService';
-dotenv.config()
-
-const supabaseUrl = process.env.SUPABASE_URL 
-const supabaseKey = process.env.SUPABASE_KEY!
-
-let main = await getData()
-
-console.log(main);
-
-
-console.log(supabaseUrl);
-console.log(supabaseKey);
-
+import { createUser } from './services/AuthenticationService';
+import { startMatch } from './services/MatchService';
 
